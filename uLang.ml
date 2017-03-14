67acd83a31629de08d43e13b90943bfd06e021a1
Mark <mark@tudders.com>
2017-03-14 18:01:40 +0000

    intersect test

--- uLang.ml
+++ uLang.ml
@@ -16,3 +16,16 @@ type uTerm =
 
 let (sets : uSet list) = [];;
 let (k : int ref) = ref 0;;
+
+
+let in_list x l =
+  let pred = function
+    | y when y==x -> true
+    | _ -> false
+    in
+    List.exists pred l;;
+
+let rec intersect l1 l2 = 
+  match l1 with
+  | hd::tl when (in_list hd l2) ->  hd::intersect tl l2
+  | hd when (in_list hd l2) -> hd;;
